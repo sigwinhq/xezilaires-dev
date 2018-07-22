718f034b88c5f9981c4df065af3f689ac94b2a36
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2018-07-22 14:24:31 +0200

    tests: initial functional tests

---
+++ tests/Xezilaires/Model/Category.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the xezilaires project.
+ *
+ * (c) Dalibor Karlović <dalibor@flexolabs.io>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Xezilaires\Test\Model;
+
+use Xezilaires\Nestable;
+
+/**
+ * Class Category.
+ */
+class Category implements Nestable
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var int
+     */
+    public $parent;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @return string
+     */
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getIdentifier()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function hasParent(): bool
+    {
+        return null !== $this->parent;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getParentIdentifier()
+    {
+        return $this->parent;
+    }
+}
